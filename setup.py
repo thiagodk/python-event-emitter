ac1ff18c663327e7b1a755b84aa08f75d71daf1d
Thiago Costa <admin@rootbsd.info>
2020-06-20 13:00:19 -0300

    Initial sources commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+    name="event_emitter",
+    version="0.1.0",
+    description="JavaScript-like EventEmitter class for Python 3",
+    author="Thiago Costa",
+    author_email="admin@rootbsd.info",
+    package_dir={"": "src"},
+    packages=["event_emitter"]
+)
