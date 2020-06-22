d60f0896d73eccf4112cdb7d32c4b67121310dc6
Thiago Costa <admin@rootbsd.info>
2020-06-22 12:24:02 -0300

    Version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="event-emitter",
-    version="0.1.0",
+    version="0.1.2",
     description="JavaScript-like EventEmitter class for Python 3",
     author="Thiago Costa",
     author_email="admin@rootbsd.info",
